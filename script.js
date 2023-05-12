a9f9972a45b0b945850ced88abd7adad399765bc
Viktar98 <129494301+Viktar98@users.noreply.github.com>
2023-05-12 12:07:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,29 +4,40 @@ let counting_Lives = 3;
 let game = true;
 let object = document.querySelector('#object');
 
-//while(game==true){
-    if(counting_Lives>=1){
-    document.title = `${lifes} Score: ${score} ${lifes}`;
-} else {document.title = `💀 Game Over 💀`;}
-//}
-
-function start_game() {
-    alert('Start');
-    object.classList.toggle = 'start';
+function title() {
+    counting_Lives == 3 ? lifes = '🖤🖤🖤' :
+        counting_Lives == 2 ? lifes = '🖤🖤🤍' :
+            counting_Lives == 1 ? lifes = '🖤🤍🤍' :
+                counting_Lives = counting_Lives;
+    if (counting_Lives >= 1) {
+        document.title = `${lifes} Score: ${score} ${lifes}`;
+    } else { document.title = `💀 Game Over 💀`; }
 }
+
+
+function start_game(){
+    score = 0;
+    object.classList.toggle('start');
+}
+
+title();
 
 function miss(event) {
     if (event.target.id == 'area') {
-        counting_Lives --;
-        counting_Lives == 3? lifes = '🖤🖤🖤': 
-        counting_Lives == 2? lifes = '🖤🖤🤍':
-        counting_Lives == 1? lifes = '🖤🤍🤍': 
-        counting_Lives = counting_Lives;
+        counting_Lives--;
     }
 }
 
+title();
+
 function hit() {
-    score ++;
-    document.title = `Score: ${score}`
+    score++;
+    object.classList.remove('start');
+    void object.offsetWidth;              //обновление DOM
+    object.classList.add('start');
+    let random_offset = Math.floor(Math.random()*7);
+    object.style.left = `${random_offset * 60}px`;
 }
+
+title();
 
